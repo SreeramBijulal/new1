72c04863c8301e13fe7afdff194050227677aa82
SreeramBijulal <164501104+SreeramBijulal@users.noreply.github.com>
2024-10-23 20:16:54 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='container-fluid p-5'>
+        <h1>Storage Management System - By Sreeram B 22BCE1291</h1>
+        <p>...</p>
+    </div>
+  )
+}
